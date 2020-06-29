23b6c7198b8b47133c41a260945c52385e8ebcac
Hussein Talal <hossientalal2@gmail.com>
2020-06-29 20:55:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(source, callback){
+  let result = [];
+  source.forEach(elem => {
+    result.push(callback(elem));
+  })
+  return result
+}
+
+function reduce(source, callback){
+  let result;
+  source.forEach(elem => {
+    result += callback(elem);
+  })
+  return result
+}
